e8ff3b096c73066950a35e3bdcb0c6b9d635a084
Madhukumar17 <146021616+Madhukumar17@users.noreply.github.com>
2024-03-17 19:38:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const MongoStore = require("connect-mongo");
 
-const dbUrl = "mongodb://127.0.0.1:27017/Yelp-Camp";
+const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/Yelp-Camp";
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
